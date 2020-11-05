6dbbbbb1e3138a07a38d84cf667ed8581bc92a3f
Muhammad Adam <muhammadalyadam@gmail.com>
2020-11-05 23:57:27 +0200

    Added test target

---
+++ TwitterCloneTests/TwitterCloneTests.swift
@@ -0,0 +1,33 @@
+//
+//  TwitterCloneTests.swift
+//  TwitterCloneTests
+//
+//  Created by Muhammad Adam on 11/5/20.
+//  Copyright © 2020 MA. All rights reserved.
+//
+
+import XCTest
+
+class TwitterCloneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
